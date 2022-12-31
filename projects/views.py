ed67bf06b7ec908b5082c1e7db6abb93b26173ba
Stephen J <sjjair@yahoo.com>
2022-12-30 22:12:01 -0800

    created user

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(req):
-    return render(req, 'projects.html')
+    return render(req, 'projects/projects.html')
 
 def project(req, pk):
-    return render(req, 'single-project.html')
+    return render(req, 'projects/single-project.html')
